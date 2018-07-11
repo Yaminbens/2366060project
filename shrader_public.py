d32f6b9fd23019906ab1717192f29107643631a8
yaminbens <yaminbs@gmail.com>
2018-07-11 14:36:40 +0300

    added rotation and flipping

--- shrader_public.py
+++ shrader_public.py
@@ -1,35 +1,36 @@
 import cv2
 import os
-import numpy as np
 
-Xa = []
-Xb = []
-y = []
+def shrader():
 
-IM_DIR = "project/images/"
-OUTPUT_DIR = "project/shraded/"
-files = os.listdir(IM_DIR)
+    Xa = []
+    Xb = []
+    y = []
 
-# update this number for 4X4 crop 2X2 or 5X5 crops.
-tiles_per_dim = 2
+    IM_DIR = "project/imagesAug/"
+    OUTPUT_DIR = "project/shraded/"
+    files = os.listdir(IM_DIR)
+
+    # update this number for 4X4 crop 2X2 or 5X5 crops.
+    tiles_per_dim = 2
 
 
 
-for f in files:
-    print(f)
-    im = cv2.imread(IM_DIR+f)
-    im = cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
-    height = im.shape[0]
-    width = im.shape[1]
-    frac_h = height//tiles_per_dim
-    frac_w = width//tiles_per_dim
-    i=0
-    for h in range(tiles_per_dim):
-        for w in range(tiles_per_dim):
+    for f in files:
+        print(f)
+        im = cv2.imread(IM_DIR+f)
+        im = cv2.cvtColor(im,cv2.COLOR_RGB2GRAY)
+        height = im.shape[0]
+        width = im.shape[1]
+        frac_h = height//tiles_per_dim
+        frac_w = width//tiles_per_dim
+        i=0
+        for h in range(tiles_per_dim):
+            for w in range(tiles_per_dim):
 
-            crop = im[h*frac_h:(h+1)*frac_h,w*frac_w:(w+1)*frac_w]
-            cv2.imwrite(OUTPUT_DIR+f[:-4]+"_{}.jpg".format(i),crop)
-            i=i+1
+                crop = im[h*frac_h:(h+1)*frac_h,w*frac_w:(w+1)*frac_w]
+                cv2.imwrite(OUTPUT_DIR+f[:-4]+"_{}.jpg".format(i),crop)
+                i=i+1
 
 
 
