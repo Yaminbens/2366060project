12546cd314147cb83c573915bc2778d072f6e935
yaminbens <yaminbs@gmail.com>
2018-07-15 15:08:50 +0300

    locc corrected

--- loss.py
+++ loss.py
@@ -12,13 +12,16 @@ def _loss_tensor(X, y_pred, y_true, CL,CR, tiles=2):
 
     X_pred = K.batch_dot(K.permute_dimensions(Xcs,(0,2,1)), y_pred) #replace column according to pred
     X_pred = K.reshape(X_pred,(X.shape[0],X.shape[-2]*tiles,X.shape[-1]*tiles)) #reshape as matrix
-    X_flip = K.reverse(K.reverse(X_pred, axes=0), axes=1)
+    X_flip_x = K.reverse(X_pred, axes=2)
+    X_flip_y = K.reverse(X_pred, axes=1)
 
-    LX = K.permute_dimensions(K.dot(CL, X_pred),(1,0,2)) + K.dot(X_pred, CR)
-    RX =K.permute_dimensions(K.dot(CL, X_flip),(1,0,2)) + K.dot(X_flip, CR)
-    dissimilarity = K.sum(K.sum(K.square(LX-RX), axis=2), axis=1)
+    LX_y = K.permute_dimensions(K.dot(CL, X_pred),(1,0,2))
+    RX_y = K.permute_dimensions(K.dot(CL, X_flip_y),(1,0,2))
+    LX_x = K.dot(X_pred, CR)
+    RX_x = K.dot(X_flip_x, CR)
+    dissimilarity = K.sum(K.sum(K.square(LX_x-RX_x), axis=2), axis=1) + K.sum(K.sum(K.square(LX_y-RX_y), axis=2), axis=1)
 
-    return  K.categorical_crossentropy(y_pred, y_true) + dissimilarity
+    return  K.categorical_crossentropy(y_pred, y_true) + K.sum(dissimilarity)
 
 
 def check_loss(_shape, tiles=2):
@@ -30,12 +33,14 @@ def check_loss(_shape, tiles=2):
         shapex = (4, 8, 6, 7)
         shapey = (8,4,4)
 
+    npixels = 3
     CL = np.zeros((shapex[-2] * tiles, shapex[-2] * tiles))
-    CL[shapex[-2], shapex[-2]] = 1
-    CL[shapex[-2] - 1, shapex[-2] - 1] = 1
+    for i in range(shapex[-2] - npixels, shapex[-2] + npixels -1):
+        CL[i, i] = 1
     CR = np.zeros((shapex[-1] * tiles, shapex[-1] * tiles))
-    CR[shapex[-1], shapex[-1]] = 1
-    CR[shapex[-1] - 1, shapex[-1] - 1] = 1
+    for i in range(shapex[-2] - npixels, shapex[-2] + npixels -1):
+        CR[i, i] = 1
+
     X = np.random.random(shapex)
     y = np.random.random(shapey)
     g = np.random.random(shapey)
